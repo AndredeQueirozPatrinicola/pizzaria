6cb5bf60ac3441b9870e553d7dfef76c7b695ef8
André de Queiroz <andrepatrinicola@gmail.com>
2022-10-16 20:03:09 -0300

    adiciona projeto react

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+
+// class App extends Component {
+
+//     render() {
+//         return (
+//             <h1>Hello Word</h1>
+//         );
+//     }
+// }
+
+function App() {
+    return (
+        <h1>Hello</h1>
+    );
+}
+
+export default App;
