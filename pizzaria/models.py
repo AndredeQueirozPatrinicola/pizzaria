6aaf94fb68d5066238d5a2caf1c9440fdcfe6bd6
André de Queiroz <andrepatrinicola@gmail.com>
2022-10-16 16:17:26 -0300

    adiciona models iniciais

---
+++ pizzaria/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from autorizacao.models import Cliente
+
+# Create your models here.
+
+
+class Sabores(models.Model):
+    nome = models.CharField(max_length=255)
+
+
+
+class Pizza(models.Model):
+    nome = models.CharField(max_length=255)
+    sabores = models.ManyToManyField(Sabores)
+    preco = models.FloatField()
+
+
+
+class Pedido(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.SET_DEFAULT, default='Pizza deletada')
+    horario = models.TimeField(auto_now=True)
+    user = models.ForeignKey(Cliente, on_delete=models.SET_DEFAULT, default='Cliente excluido')
