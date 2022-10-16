dddc672e0df26c9848e13bbc3e571da2a59f789d
André de Queiroz <andrepatrinicola@gmail.com>
2022-10-16 16:37:16 -0300

    adiciona views para teste

--- pizzaria/views.py
+++ pizzaria/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.http.response import HttpResponse
 
 # Create your views here.
+
+
+# class Index(ListView):
+
+def index(request):
+    return HttpResponse('<h1>Teste</h1>')
