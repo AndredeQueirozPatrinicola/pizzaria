ac3cf709350d1054108a70254ed5dfa698b5084f
André de Queiroz <andrepatrinicola@gmail.com>
2022-12-13 10:44:12 -0300

    corrige disposicao dos elementos no cardapio

--- frontend/piulzza/src/components/CardPizza/index.js
+++ frontend/piulzza/src/components/CardPizza/index.js
@@ -3,6 +3,7 @@ import placeholder from './pizza-placeholder.jpg';
 import placeholder2 from './frango.jpg';
 import placeholder3 from './pizza-templ.jpg';
 import React from "react";
+import { TailSpin } from "react-loader-spinner";
 
 class CardPizza extends React.Component {
 
@@ -17,7 +18,7 @@ class CardPizza extends React.Component {
 
     componentDidMount() {
         fetch(
-          "http://127.0.0.1:8000/api/pizza/")
+            "http://127.0.0.1:8000/api/pizza/")
             .then((res) => res.json())
             .then((json) => {
                 this.setState({
@@ -29,37 +30,51 @@ class CardPizza extends React.Component {
     render() {
         const array = [placeholder, placeholder2, placeholder3];
         const { DataisLoaded, items } = this.state;
-        if (!DataisLoaded) return <div>
-            <h1> Pleses wait some time.... </h1> </div> ;
-
-        return (
-        <div className = "card-pizza-main">
-            {
-                items.map((item) => ( 
-                <ol key = { item.id } className="card-pizza">
-                    <img className='imagem-card-pizza' src={ array[Math.floor(Math.random() * array.length)] } alt="Imagem pizza"></img>
-                    <li className='nome-pizza'>{ item.nome }</li>
-                        <div>
-                        {
-                            item.sabores.map((sabor)=>
-                            {
-                                if(item.sabores.indexOf(sabor) === item.sabores.length)
-                                {
-                                    return <li key={ sabor.id } className='sabores'>{ sabor.nome }.</li>
-                                }
-                                else
-                                {
-                                    return <li key={ sabor.id } className='sabores'>{ sabor.nome }, </li>
-                                }
-                            })
-                        }
-                    </div>
-                    </ol>
-                ))
-            }
-        </div>
-    );
-}
+        if (!DataisLoaded) {
+            return (
+                <div className='spinner-card-pizza'>
+                    <TailSpin
+                        height="80"
+                        width="80"
+                        color="#840d23"
+                        ariaLabel="tail-spin-loading"
+                        radius="1"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                        visible={true}
+                    />
+                </div>
+            );
+        }
+        else {
+            return (
+                <div className="card-pizza-main">
+                    {
+                        items.map((item) => {
+                                return (
+                                    <ol key={item.id} className="card-pizza">
+                                        <img className='imagem-card-pizza' src={array[Math.floor(Math.random() * array.length)]} alt="Imagem pizza"></img>
+                                        <li className='nome-pizza'>{item.nome}</li>
+                                        <div>
+                                            {
+                                                item.sabores.map((sabor) => {
+                                                    if (item.sabores.indexOf(sabor) === item.sabores.length) {
+                                                        return <li key={sabor.id} className='sabores'>{sabor.nome}.</li>
+                                                    }
+                                                    else {
+                                                        return <li key={sabor.id} className='sabores'>{sabor.nome}, </li>
+                                                    }
+                                                })
+                                            }
+                                        </div>
+                                    </ol>
+                                )
+                        })
+                    }
+                </div>
+            );
+        }
+    }
 }
 
 export default CardPizza;
