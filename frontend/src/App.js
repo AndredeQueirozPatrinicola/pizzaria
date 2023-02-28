51cc2c1054ae62380eee2e40360326fc2446a07a
André de Queiroz <andrepatrinicola@gmail.com>
2023-02-28 00:38:25 -0300

    adiciona axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  axios.get('/api/pizza/')
+  .then(function (response) {
+      console.log(response)
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    console.log("tei")
+  });
+
     return (
       <div className="App">
         Coco
